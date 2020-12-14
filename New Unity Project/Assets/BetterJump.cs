2d1b0f1b7c10548d1f8b94610a3a71b828580f0b
ianitalo <ianitalo56@hotmail.com>
2020-12-14 21:55:20 +0000

    idea
    uma ideia basica pro primeiro nivel

---
+++ New Unity Project/Assets/BetterJump.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterJump : MonoBehaviour
+{
+    [SerializeField] private float jFallmult = 40;
+    [SerializeField] private float lowjumpmult = 40;
+
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb.GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (jFallmult - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowjumpmult - 1) * Time.deltaTime;
+        }
+    }
+}
--- New Unity Project/Assets/Control.cs
+++ New Unity Project/Assets/Control.cs
@@ -4,21 +4,89 @@ using UnityEngine;
 
 public class Control : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private enum State {idle, runnning, jump, falling}
+    private State state = State.idle;
+    private Collider2D coll;
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jspeed = 40;
+    
+
+    public int dimas = 0;
+
+    private void Start() 
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
+    }
+    
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        Movement();
+        VelocityState();
+        anim.SetInteger("state", (int)state);
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Collectable")
         {
-            rb.velocity = new Vector2(-5, rb.velocity.y);
+            Destroy(collision.gameObject);
+            dimas += 1;
         }
-        if (Input.GetKey(KeyCode.D))
+    }
+    private void Movement()
+    {
+        float hDirection = Input.GetAxis("Horizontal");
+        if (hDirection < 0)
         {
-            rb.velocity = new Vector2(5, rb.velocity.y);
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
+
         }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {  
-            rb.velocity = new Vector2(rb.velocity.x, 40);
+        else if (hDirection > 0)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
+
+        }
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jspeed);
+            state = State.jump;
+        }
+        
+    }
+    private void VelocityState()
+    {
+        if (state == State.jump)
+        {
+            if(rb.velocity.y < 0.1)
+            {
+                state = State.falling;
+            }
+        }
+        else if (state == State.falling)
+        {
+            if (coll.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+                   
+        }
+        else if (Mathf.Abs(rb.velocity.x) > 0.1)
+        {
+            state = State.runnning;
+        } 
+
+
+        else
+        {
+            state = State.idle;
         }
     }
 }
